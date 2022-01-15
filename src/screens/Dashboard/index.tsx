c1b103f5c19107efdfbbdbe62ff81dedea750753
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-01-15 16:47:32 -0400

    create header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,35 @@
 import React from "react";
-import { Container, Title } from "./styles";
+
+import {
+    Container,
+    Header,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    UserWrapper,
+    Icon
+} from "./styles";
 
 export function Dashboard() {
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/43017908?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Josilene</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <Icon name="power" />
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }

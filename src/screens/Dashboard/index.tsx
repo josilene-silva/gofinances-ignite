1005921904bc0825eaa8b19d63fcc6bf952817ea
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-01-15 18:07:19 -0400

    create highlight cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
 
 import {
     Container,
@@ -9,7 +10,8 @@ import {
     UserGreeting,
     UserName,
     UserWrapper,
-    Icon
+    Icon,
+    HighlightCards
 } from "./styles";
 
 export function Dashboard() {
@@ -30,6 +32,24 @@ export function Dashboard() {
                     <Icon name="power" />
                 </UserWrapper>
             </Header>
+
+            <HighlightCards>
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril"/>
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última saída dia 03 de abril"/>
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 à 16 de abril"/>
+            </HighlightCards>
         </Container>
     )
 }

d5ff50827d3b8d90f24573c03ebf9dc6c2d73b10
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-01-17 21:33:50 -0400

    add tab navigator

--- App.tsx
+++ App.tsx
@@ -9,9 +9,9 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
-import { Register } from './src/screens/Register';
-import { CategorySelect } from './src/screens/CategorySelect';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded]=  useFonts({
@@ -26,7 +26,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
